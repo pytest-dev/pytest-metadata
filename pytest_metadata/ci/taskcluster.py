ea280671e324c1786b08aa6521265822e8269c4d
Jim Brännlund <BeyondEvil@users.noreply.github.com>
2018-11-13 14:12:24 +0100

    Make project Blacked (#11)

--- pytest_metadata/ci/taskcluster.py
+++ pytest_metadata/ci/taskcluster.py
@@ -2,6 +2,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ENVIRONMENT_VARIABLES = [
-    'TASK_ID',
-    'RUN_ID']
+ENVIRONMENT_VARIABLES = ["TASK_ID", "RUN_ID"]
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -2,61 +2,69 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pytest_plugins = "pytester",
+pytest_plugins = ("pytester",)
 
 
 def test_metadata(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         def test_pass(metadata):
             for k in ['Python', 'Platform', 'Packages']:
                 assert k in metadata
                 assert 'JENKINS_URL' not in metadata
-    """)
+    """
+    )
     result = testdir.runpytest()
     assert result.ret == 0
 
 
 def test_environment_variables(testdir, monkeypatch):
-    monkeypatch.setenv('JENKINS_URL', 'foo')
-    monkeypatch.setenv('GIT_COMMIT', 'bar')
-    testdir.makepyfile("""
+    monkeypatch.setenv("JENKINS_URL", "foo")
+    monkeypatch.setenv("GIT_COMMIT", "bar")
+    testdir.makepyfile(
+        """
         def test_pass(metadata):
             assert metadata.get('JENKINS_URL') == 'foo'
             assert metadata.get('GIT_COMMIT') == 'bar'
-    """)
+    """
+    )
     result = testdir.runpytest()
     assert result.ret == 0
 
 
 def test_additional_metadata(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         def test_pass(metadata):
             assert metadata.get('Dave') == 'Hunt'
             assert metadata.get('Jim') == 'Bob'
-    """)
-    result = testdir.runpytest('--metadata', 'Dave', 'Hunt',
-                               '--metadata', 'Jim', 'Bob')
+    """
+    )
+    result = testdir.runpytest("--metadata", "Dave", "Hunt", "--metadata", "Jim", "Bob")
     assert result.ret == 0
 
 
 def test_metadata_hook(testdir):
-    testdir.makeconftest("""
+    testdir.makeconftest(
+        """
         import pytest
         @pytest.mark.optionalhook
         def pytest_metadata(metadata):
             metadata['Dave'] = 'Hunt'
-    """)
-    testdir.makepyfile("""
+    """
+    )
+    testdir.makepyfile(
+        """
         def test_pass(metadata):
             assert metadata.get('Dave') == 'Hunt'
-    """)
+    """
+    )
     result = testdir.runpytest()
     assert result.ret == 0
 
 
 def test_report_header(testdir):
     result = testdir.runpytest()
-    assert not any(line.startswith('metadata:') for line in
-                   result.stdout.lines)
-    result = testdir.runpytest('-v')
-    assert any(line.startswith('metadata:') for line in result.stdout.lines)
+    assert not any(line.startswith("metadata:") for line in result.stdout.lines)
+    result = testdir.runpytest("-v")
+    assert any(line.startswith("metadata:") for line in result.stdout.lines)
